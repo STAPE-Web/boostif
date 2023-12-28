b0018deb67dd0cd4971cdd3de5117f7b2f5857e8
Kirill Kirilenko <ggggfetfdfgjjh2@gmail.com>
2023-12-28 19:34:48 +0500

    update

--- src/components/TrustBox/index.tsx
+++ src/components/TrustBox/index.tsx
@@ -9,14 +9,13 @@ const TrustpilotWidget = () => {
         document.body.appendChild(script);
 
         return () => {
-            // Очистка скрипта при размонтировании компонента
             document.body.removeChild(script);
         };
     }, []);
 
     return (
         <div className="trustpilot-widget" data-locale="en-US" data-template-id="5613c9abf85e702406ab8c02" data-businessunit-id="6572fadaba14ca001bdaec13" data-style-height="130px" data-style-width="100%" data-theme="light" data-stars="4,5">
-            <a href="https://www.trustpilot.com/review/YOUR_DOMAIN" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.trustpilot.com/review/boostif.com" target="_blank" rel="noopener noreferrer">
                 Trustpilot
             </a>
         </div>
